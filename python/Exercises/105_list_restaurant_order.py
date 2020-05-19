9eb291d3e2ad4d7b29f0449ca98115d9703523d4
Ryan Smith <smithy.ryan96@hotmail.co.uk>
2020-05-19 16:42:49 +0100

    added annotations

--- python/Exercises/105_list_restaurant_order.py
+++ python/Exercises/105_list_restaurant_order.py
@@ -20,17 +20,25 @@ import time
 
 # starter code
 # USER STORY 1
+
 menu = ['Pizza', 'Burger', 'Enchiladas', 'Quesadillas', 'Just Cake :-)']
 food_order = []
 print("Here is today's Menu!!")
+# Loop through menu printing out the item number (not index) and the item itself so the customer knows what to order
+
 for (x, item) in enumerate(menu, start=1):
     print(x, item)
-time.sleep(2)
+
+time.sleep(2)  # Delay for two seconds
 # USER STORY 2
+
 print("What would you like to order today? Use the numbers in the list to pick an item!")
+# Get user to enter number of item (using list) that they wish to order
 item_1 = input("What will be the first item? \n")
-food_order.append(item_1)
-time.sleep(1)
+food_order.append(item_1)  # add number to food order list
+time.sleep(1)  # delay 1 second
+
+# repeat for second and third item of the customers order
 item_2 = input("And the second? \n")
 food_order.append(item_2)
 time.sleep(1)
@@ -38,11 +46,18 @@ item_3 = input("And the final item? \n")
 food_order.append(item_3)
 
 # USER STORY 3
+# Tell user that their order is confirmed
 print("Your order is IN! Confirming...")
 time.sleep(2)
+
+# Loop through the food order list (3 times)
 for x in food_order:
+    # Loop again this time using the length of the menu as the parameter
+    # (to be able to get the positions of items in the menu)
     for i in range(len(menu)):
+        # Check if number entered is matched with a position in the menu array
         if int(x) == (i+1):
+            # if matched - print out the menu item
            print(menu[i])
 
 # I need to print each item from the list
@@ -50,4 +65,4 @@ for x in food_order:
 # before I print I need to make them all capitalized
 # print everything
 for x in menu:
-    print(x.upper())
+    print(x.upper()) # convert menu items to upper case
