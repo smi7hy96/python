77ca7e88fe0b9bb357191076fb97b97e9c38dbde
Ryan Smith <smithy.ryan96@hotmail.co.uk>
2020-05-26 15:45:40 +0100

    changes to 108 - added functions

--- python/Exercises/108_mr_miyagi.py
+++ python/Exercises/108_mr_miyagi.py
@@ -2,21 +2,11 @@
 
 # make a Mr Miyagi virtual assistant
 
-# as a user I should be able to speak with Mr Miyagi and get appropriate response s as I go
-print("MR MIYAGI VIRTUAL ASSISTANT \n")
 
-name = input("Enter your name \n")
-print("Say Something Below " + name + ":")
-# Ask for user input and depending on the response, Mr Miyagi will respond.
-end_code = False
-
-#  make it run continuously
-while not end_code:
+# put main body of code in a function
+def miyagi_response(user_input):
     response_count = 0
-    # prompt user for input
-    user_input = input("[Mr Miyagi] Yes???\n" + "[" + name + "] ").lower()
-    # Make it so you keep playing until we say: 'Sensei, I am at peace'
-    # --> 'Sometimes, what heart know, head forget'
+    # Evaluate each input and print the appropriate responses
     if user_input == "sensei, i am at peace":
         end_code = True
         print("[Mr Miyagi] Sometimes, what heart know, head forget")
@@ -29,7 +19,7 @@ while not end_code:
             # every time you ask a question --> Mr. Miyagi responde with
             # --> 'questions are wise, but for now. Wax on, and Wax off!'
         if user_input.strip().endswith('?'):
-            print("[Mr Miyagi] Questions are wise, but for now. Wax onm Wax off!")
+            print("[Mr Miyagi] Questions are wise, but for now. Wax on. Wax off!")
             response_count += 1
             # every time you mention 'block' or 'blocking' --> Mr. Miyagi responde with
             # --> 'Remember, best block, not to be there..'
@@ -40,7 +30,23 @@ while not end_code:
             # --> 'do not lose focus. Wax on. Wax off.'
         if response_count == 0:
             print("[Mr Miyagi] Do not lose focus. Wax on. Wax off.")
-# Evaluate each input and print the appropriate responses
+
+
+# as a user I should be able to speak with Mr Miyagi and get appropriate response s as I go
+print("MR MIYAGI VIRTUAL ASSISTANT \n")
+name = input("Enter your name \n")
+print("Say Something Below " + name + ":")
+# Ask for user input and depending on the response, Mr Miyagi will respond.
+
+end_code = False
+#  make it run continuously
+while not end_code:
+    # prompt user for input
+    user_input = input("[Mr Miyagi] Yes???\n" + "[" + name + "] ").lower()
+    miyagi_response(user_input)  # call function
+    # Make it so you keep playing until we say: 'Sensei, I am at peace'
+    # --> 'Sometimes, what heart know, head forget'
+
 
 
 #  EXTRA:
