02be8e7029c30f9359cfedaf6e103d7c0473c675
Ryan Smith <smithy.ryan96@hotmail.co.uk>
2020-05-27 10:42:38 +0100

    random

--- python/Exercises/104_Magic_Number.py
+++ python/Exercises/104_Magic_Number.py
@@ -2,10 +2,10 @@
 # I want you to use operators
 # equate something
 # As a user, I want to be able to guess a number and know if i got it correct or not, so that I can know if I won or not.
-
+import random
 
 # We should define/assign number to a variable called magic_number
-magic_number = 7
+magic_number = random.randint(1, 10)
 
 # I need to ask user for input
 user_guess = int(input("Guess which number is the magic number! \n"))
@@ -14,7 +14,8 @@ user_guess = int(input("Guess which number is the magic number! \n"))
 if user_guess == magic_number:
     print("YOU GUESSED RIGHT! CONGRATULATIONS")
 else:
-    print("Ah. Wrong answer, better luck next time!")
+    print(f'Ah. Wrong answer, better luck next time! (Answer was {magic_number})')
 
-# I need to let the user know if the response was write or not
+# I need to let the user know if the response was right or not
 #self five
+
