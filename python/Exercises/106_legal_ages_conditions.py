1ea94717ea57ebd69b4cac5a94a3ab78b886e1b3
Ryan Smith <smithy.ryan96@hotmail.co.uk>
2020-05-27 13:43:47 +0100

    added function to 106

--- python/Exercises/106_legal_ages_conditions.py
+++ python/Exercises/106_legal_ages_conditions.py
@@ -7,20 +7,24 @@ driver_license = True
 # - You can driver
 # - you can't legally drink but your mates/uncles might have your back (bigger 16)
 # - Your too young, go back to school!
-new_age = int(input("How old are you? \n"))
-if new_age >= 18:
-    print("You can drink, drive and vote (BUT NOT AT THE SAME TIME PLEASE!)")
-elif new_age == 17:
-    print("You can vote and drive but you can't drink sorry!")
-elif new_age == 16:
-    print("You can vote now but got to wait before you can do anything else")
-else:
-    print("You're too young! Go back to school!")
+def age_response(new_age):
+    if new_age >= 18:
+        print("You can drink, drive and vote (BUT NOT AT THE SAME TIME PLEASE!)")
+    elif new_age == 17:
+        print("You can vote and drive but you can't drink sorry!")
+    elif new_age == 16:
+        print("You can vote now but got to wait before you can do anything else")
+    else:
+        print("You're too young! Go back to school!")
 
  # as a user I should be able to keep being prompted for input until I say 'exit'
 exit_boolean = False
 while not exit_boolean:
-    user_input = input("Type 'exit' when you want to quit \n").lower()
-    if user_input == "exit":
-        exit_boolean = True
+    user_input = input("How old are you? (Type 'exit' when you want to quit) \n").lower()
+
+    if user_input.isnumeric():
+        age_response(int(user_input))
+    else:
+        if user_input == "exit":
+            exit_boolean = True
 print("EXIT COMPLETE")
