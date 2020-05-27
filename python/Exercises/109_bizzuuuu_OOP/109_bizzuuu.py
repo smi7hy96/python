48c0448038468abe748e67d4d968fb34fc9d6b9a
Ryan Smith <smithy.ryan96@hotmail.co.uk>
2020-05-27 10:33:26 +0100

    added annotations to OO bizzu exercise

--- python/Exercises/109_bizzuuuu_OOP/109_bizzuuu.py
+++ python/Exercises/109_bizzuuuu_OOP/109_bizzuuu.py
@@ -21,23 +21,27 @@ from Game import *
 game_1 = Game(None, None, None, None)
 
 print("WELCOME! How would you like to play BIZZZZUU?: \n 1) Standard (3 and 5) \n 2) Bigger (4 and 9) \n 3) Custom "
-      "(Pick your own!)")
+      "(Pick your own!)")  # welcome message
 correct_input = False
-while not correct_input:
-    user_choice = input("Enter option 1,2 or 3 to play! \n")
-    if game_1.check_if_number(user_choice):
-        x = int(user_choice)
-        if x == 1 or x == 2 or x ==3:
-            correct_input = True
-            game_1.type = x
-            game_1.play()
+while not correct_input:  # check input while loop. keeps repeating until condition true
+    user_choice = input("Enter option 1,2 or 3 to play! \n")  # ask for input
+    if game_1.check_if_number(user_choice):  # check if input is actually a number
+        x = int(user_choice)  # now confirmed, convert input type to integer
+        if x == 1 or x == 2 or x ==3:  # check that number entered is one of the 3 expected inputs
+            correct_input = True  # if it is, set true and break while loop
+            game_1.type = x  # set the object type as the user input
+            game_1.play()  # execute function play, in Game.py
         else:
-            "Invalid Option. Try Again"
+            "Invalid Option. Try Again"  # error message
 
-game_1.bizzzzuu_me()
+print(game_1.bizzzzuu_me())  # prints the list returned from the method,can print every item on a separately if needed
+# (see commented code below)
+# bzu_list = game_1.bizzzzuu_me()  # for clarity, store returned list as new variable
+# for item in bzu_list:  # loop through each item in list
+#     print(item)  # print item
 
 
-print("Thanks for playing!")
+print("Thanks for playing!")  # Thanks you message
 
 
 
--- python/Exercises/109_bizzuuuu_OOP/Game.py
+++ python/Exercises/109_bizzuuuu_OOP/Game.py
@@ -1,56 +1,60 @@
 class Game:
 
-    def __init__(self, type, limit, cond_1, cond_2):
-        self.type = type
+    def __init__(self, type, limit, cond_1, cond_2):  # initailise object instance, 4 params
+        self.type = type  # set the parameters
         self.limit = limit
         self.cond_1 = cond_1
         self.cond_2 = cond_2
 
-    def check_if_number(self, number):
-        if number.isnumeric() and int(number) >= 1:
-            return True
+    def check_if_number(self, number):  # validation check to see if input is a number or not
+        if number.isnumeric() and int(number) >= 1:  # is numeric checks that param is a number 0-9.
+            # and also must be greater than 0
+            return True  # return true to confirm it is a number
         else:
-            print("You haven't typed in a valid number, please try again!")
-            return False
+            print("You haven't typed in a valid number, please try again!")  # error message
+            return False  # not a number, return false
 
-    def get_params(self):
+    def get_params(self):  # function for setting the boundary numbers
         correct_input1 = False
-        while not correct_input1:
-            if self.cond_1 is None:
-                param = input("Pick a the lowest number: ")
-            else:
-                param = input("Pick a the highest number: ")
-            if self.check_if_number(param) and int(param) != self.cond_1:
+        while not correct_input1:  # while loop to repeat picking numbers until two different numbers have been selected
+            if self.cond_1 is None:  # check that the attribute of current object is empty
+                param = input("Pick the lowest number: ")  # input number for the lower boundary
+            else:  # if lower boundary already has a value, then ask for higher number
+                param = input("Pick the highest number: ")
+            if self.check_if_number(param) and int(param) != self.cond_1:  # check input with if number function
+                # and also check that the number entered isn't equal to the value stored in the first boundary location
                 correct_input1 = True
-                return int(param)
+                return int(param)  # return the number
             else:
-                print("Number is the same as first parameter - not allowed")
+                print("Number is the same as first parameter - not allowed")  # error message
 
-    def bizzzzuu_me(self):
-        for x in range(int(self.limit)):
-            y = x + 1
-            if y % self.cond_1 == 0 and y % self.cond_2 == 0:  # if divisible by both 3 and 5, print fizzbuzz
-                print("bizzzzuu")
-            elif y % self.cond_1 == 0:  # if only divisible by 3, print fizz
-                print("bizz")
-            elif y % self.cond_2 == 0:  # if only divisible by 5, print buzz
-                print("zzuu")
+    def bizzzzuu_me(self):  # final function that prints all the numbers. doesn't need parameters
+        bzu_list = []
+        for x in range(int(self.limit)):  # for loop to repeat code a set number of times (the limit entered by user)
+            y = x + 1  # loop count (x) starts at 0 instead of 1 so corrects that
+            if y % self.cond_1 == 0 and y % self.cond_2 == 0:  # if divisible by both 3 and 5, add fizzbuzz to the list
+                bzu_list.append("bizzzzuu")
+            elif y % self.cond_1 == 0:  # if only divisible by 3, add fizz
+                bzu_list.append("bizz")
+            elif y % self.cond_2 == 0:  # if only divisible by 5, add buzz
+                bzu_list.append("zzuu")
             else:
-                print(y)  # if none of those, just print the number
+                bzu_list.append(y)  # if none of those, just add the number
+        return bzu_list  # return the list
 
-    def play(self):
+    def play(self):  # initial function that sets most of the attributes of the object
         correct_input = False
-        while not correct_input:
-            user_number = input("Please enter the limit number now!\n")
-            if self.check_if_number(user_number):
-                self.limit = user_number
-                correct_input = True
-                if self.type == 1:
-                    self.cond_1 = 3
-                    self.cond_2 = 5
-                elif self.type == 2:
+        while not correct_input:  # while loop to keep repeating until valid input has been entered
+            user_number = input("Please enter the limit number now!\n")  # user enters max number
+            if self.check_if_number(user_number):  # check if number
+                self.limit = user_number  # if true, set number as limit in attribute
+                correct_input = True  # return true, break while loop
+                if self.type == 1:  # checking type (set in another method) there are only 3 types (1,2 or 3)
+                    self.cond_1 = 3  # set boundary 1
+                    self.cond_2 = 5  # set boundary 2
+                elif self.type == 2:  # same again but with different fixed numbers this time
                     self.cond_1 = 4
                     self.cond_2 = 9
-                else:
-                    self.cond_1 = self.get_params()
-                    self.cond_2 = self.get_params()
+                else:  # if neither, then the game type has custom boundaries.
+                    self.cond_1 = self.get_params()  # use get_params function to set attribute with custom number
+                    self.cond_2 = self.get_params()  # same as above but with the other boundary attribute
